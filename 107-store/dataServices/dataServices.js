f37917407926881bcbd78048d7bf90942d2bbc4c
TheJessDev <121843183+TheJessDev@users.noreply.github.com>
2023-03-11 11:24:00 -0700

    Add files via upload

---
+++ 107-store/dataServices/dataServices.js
@@ -0,0 +1,15 @@
+var catalog = [
+    {
+        "title":"Shoe",
+        "category":"Shoes",
+        "price":39.99,
+    },
+];
+
+class DataServices {
+    getProducts() {
+        return catalog;
+    }
+}
+
+export default DataServices;
